9cce0f38c5377120eac31240b8b06b9ed8371d62
JBin <shine3199@naver.com>
2017-07-25 11:05:37 +0900

    no message

--- Problem/src/problem/if_/p4344.java
+++ Problem/src/problem/if_/p4344.java
@@ -9,6 +9,7 @@ public class p4344 {
 		
 		try{
 			int c = in.nextInt();
+			in.nextLine();
 			String[][] s = new String[c][];
 			for(int i = 0 ; i < c ; i++){
 				s[i] = in.nextLine().split(" ");
@@ -16,38 +17,30 @@ public class p4344 {
 			for(int i = 0 ; i < c ; i++){
 				float result = 0;
 				for(int j = 0; j < Integer.parseInt(s[i][0]) ; j++){
-					result += (float)Integer.parseInt(s[i][j+1]);
+					result += Float.parseFloat(s[i][j+1]);
 				}
-				result /= (float)Integer.parseInt(s[i][0]);
-				
+				result /= Float.parseFloat(s[i][0]);
+				float cnt = 0;
+				for(int j = 0; j < Integer.parseInt(s[i][0]) ; j++){
+					if(Float.parseFloat(s[i][j+1]) > result){
+						cnt++;
+					}
+				}
+				System.out.println(String.format("%.3f" ,cnt/Float.parseFloat(s[i][0])*100)+"%");
 			}
-			
 		}finally {
 			in.close();
 			in = null;
 		}
 	}
-
 }
-//문제
-//대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
-//
-//입력
-//첫째 줄에는 테스트케이스 C가 주어진다.
-//
-//둘째 줄부터 각 테스트케이스 마다 첫 수로 정수 N(1 <= N <= 1000)명의 학생이 주어지고 그 다음으로 N명의 0부터 100 사이의 점수가 이어서 주어진다.
-//
-//출력
-//각 케이스마다 한줄씩 평균을 넘는 학생들의 비율을 소수점 넷째자리에서 반올림하여 출력한다.
-//
-//예제 입력  복사
 //5
 //5 50 50 70 80 100
 //7 100 95 90 80 70 60 50
 //3 70 90 80
 //3 70 90 81
 //9 100 99 98 97 96 95 94 93 91
-//예제 출력  복사
+
 //40.000%
 //57.143%
 //33.333%
