a567816a5e03402ef0566b043b3d77e808fb2840
Park JiBin <shine3199@naver.com>
2017-08-02 16:53:46 +0900

    no message

---
+++ Problem/src/problem/cal/P10869.java
@@ -0,0 +1,17 @@
+package problem.cal;
+
+import java.util.Scanner;
+
+public class P10869 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
